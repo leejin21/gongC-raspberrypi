35601480059e38bd68fa8fa062558772ed3a9f62
Jin Lee <leejin21c@gmail.com>
2021-11-01 16:23:03 +0900

    fix(model): torch 실험: torch.hub.load 명령어 에러 관련

--- app.py
+++ app.py
@@ -36,6 +36,17 @@ def main():
 
 if __name__== "__main__":
     # 해당 파일 실행 시 실행
-    model = torch.hub.load('yolov5', 'custom', path='/gongC-raspberrypi/weights/best.pt', source='local', force_reload=True)
-    main()
+    # Model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+    # Image
+    img = 'https://ultralytics.com/images/zidane.jpg'
+
+    # Inference
+    results = model(img)
+
+    results.pandas().xyxy[0]
+    # model = torch.hub.load('yolov5', 'custom', path='/gongC-raspberrypi/weights/best.pt', source='local', force_reload=True)
+    
+    # main()
     pass
