ef4e7508e8ae53aaaf3f2d75dc7d526ee3d8a063
Jin Lee <leejin21c@gmail.com>
2021-11-01 16:07:45 +0900

    feat(model): 웹캠 & 커스텀 yolo model 연결 후 결과값 로그로 도출

---
+++ app.py
@@ -0,0 +1,41 @@
+import requests
+import torch
+import numpy as np
+import cv2
+import json
+
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+def main():
+    global model
+    '''
+    [기능]
+    1. 5초마다 캠으로 집중 여부 파악하기
+        (1) DUMMY: 로그로 표시(객체 -> 객체 간 근접 여부 -> 집중 여부)
+        (2) REAL: 집중 여부 LED로 표시
+    2. 60초마다 축적한 12개의 집중 여부 -> 서버로 전송
+    3. (확장) 전광판 사용하기
+    '''
+    stream = cv2.VideoCapture(0)
+    # 0 means read from local camera
+    results = model(stream)
+    results.print()
+    pass
+
+
+if __name__== "__main__":
+    # 해당 파일 실행 시 실행
+    model = torch.hub.load('yolov5', 'custom', path='/weights/best.pt', source='local')
+    main()
+    pass
