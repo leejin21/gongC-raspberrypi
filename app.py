12c708a7f921d70b8dd8eeba610fa53e99a7aa82
JinLee <41279487+leejin21@users.noreply.github.com>
2021-11-10 23:45:23 +0900

    [Feature/model] 기능 1.2.1 - 커스텀 모델 적용 후 콘솔에 로깅 완, 기능 1.2.2 - 서버에 1분마다 POST 요청 틀만 잡음 (#1)
    * fix(model): FileNotFoundError fix try 2
    
    * feat(model): FileNotFoundError fix try 3
    
    * fix(model): FileNotFoundError fix try 4
    
    * feat(model): 웹캠 스트림 적용
    
    * feat(model): 기능 1.2 웹캠 frame별로 모델 적용하기
    
    * fix(기능-1.2.1): FIX NameError: name 'time' is not defined
    
    * feat(기능-1.2.1): 웹캠 frame별로 모델 적용하기 > 로그 좀 더 자세하게
    
    * fix(기능-1.2.1): FIX Attribute Error 'str' object has no attr 'name'
    
    * feat(기능-1.2.1): 로깅 더 자세히
    
    * fix(기능-1.2.1): FIX AttributeError: 'dict' object has no attribute 'name'
    
    * feat(기능-1.2.1): confidence 소수점 아래 셋째자리까지 반올림
    
    * feat(기능-1.2.1): 현재 시간도 로깅하도록
    
    * feat(기능-1.2.1): 현재 시간 로깅할 때 HH:MM 형태로
    
    * Revert "feat(기능-1.2.1): 현재 시간 로깅할 때 HH:MM 형태로"
    This reverts commit 64d3c53b027a8b71fff7c13d3e40466935f970d3.
    
    * chore(init): 라즈베리파이 기본 세팅
    
    * feat(기능-1.2.1) 현재 시간 로깅할 때 HH:MM:SS 로
    
    * feat(기능-1.2.2): 분 바뀔 때마다 로그 찍기
    
    * feat(기능-1.2.2): 분 바뀔 때마다 request 보낼 코드 틀 잡기
    
    * feat(기능-1.2.2): 분 바뀔 때마다 POST 성공 로그 찍는 부분 = 에서 * 로 수정

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import requests
+import requests, json
 import torch
 import numpy as np
 import cv2
 import json
+import time
 
 def convert(size, box):
     dw = 1./size[0]
@@ -17,25 +18,7 @@ def convert(size, box):
     h = h*dh
     return (x,y,w,h)
 
-def main():
-    global model
-    '''
-    [기능]
-    1. 5초마다 캠으로 집중 여부 파악하기
-        (1) DUMMY: 로그로 표시(객체 -> 객체 간 근접 여부 -> 집중 여부)
-        (2) REAL: 집중 여부 LED로 표시
-    2. 60초마다 축적한 12개의 집중 여부 -> 서버로 전송
-    3. (확장) 전광판 사용하기
-    '''
-    stream = cv2.VideoCapture(0)
-    # 0 means read from local camera
-    results = model(stream)
-    results.print()
-    pass
-
-
-if __name__== "__main__":
-    # 해당 파일 실행 시 실행
+def test():
     # Model
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
@@ -46,7 +29,81 @@ if __name__== "__main__":
     results = model(img)
 
     print(results.pandas().xyxy[0])
-    # model = torch.hub.load('yolov5', 'custom', path='/gongC-raspberrypi/weights/best.pt', source='local', force_reload=True)
+
+def getResultArray(results):
+    '''
+    [OUTPUT 형태]
+    [dict 객체, dict 객체, ...]
+    예시: [{'xmin': 226.7082824707, 'ymin': 210.1504211426, 'xmax': 561.6719970703, 'ymax': 480.0, 'confidence': 0.5927491188, 'class': 0, 'name': 'handonly'}, {'xmin': 300.7040100098, 'ymin': 30.6222915649, 'xmax': 479.7358703613, 'ymax': 270.3709106445, 'confidence': 0.2770100236, 'class': 9, 'name': 'phone'}]
+    '''
+    results_array = results.pandas().xyxy[0].to_json(orient="records")      # 결과값 json변환
+    results_array = results_array.replace("'","\"")     # '을 "로 치환해야 json으로 변환 가능함
+    results_array = json.loads(results_array)       # string을 json(dict)형식으로 변환
+    return results_array
+
+def postDataBy1Min(data):
+    URL = 'http://www.tistory.com'
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    cookies = {'session_id': 'sorryidontcare'}
+    # res = requests.post(URL, data, headers=headers, cookies=cookies)
+    print("*"*50)
+    print("POST /concent/data 성공")
+    print("*"*50)
+    ## TODO 성공/실패 각각 대응 코드 작성
+    # return res
     
-    # main()
+def main():
+    global model
+    '''
+    [기능]
+    1. 5초마다 캠으로 집중 여부 파악하기
+        (1) DUMMY: 로그로 표시(객체 -> 객체 간 근접 여부 -> 집중 여부)
+        (2) REAL: 집중 여부 LED로 표시
+    2. 60초마다 축적한 12개의 집중 여부 -> 서버로 전송
+    3. (확장) 전광판 사용하기
+    '''
+    player = cv2.VideoCapture(0)
+    assert player.isOpened()
+    # 0 means read from local camera
+    current_time = {"hour": time.localtime().tm_hour, "minute": str(time.localtime().tm_min)}
+    study_data = {"C": 0, "P":0}
+
+    while True:
+        prev_minute = current_time["minute"]
+
+        start_time = time.time()
+        ret, frame = player.read()
+        assert ret
+        results = model(frame)
+        end_time = time.time()
+
+        fps = 1/np.round(end_time - start_time, 3)
+        
+        # 분이 바뀔 때마다 서버에 POST 요청
+        current_time = {"hour": time.localtime().tm_hour, "minute": str(time.localtime().tm_min)}
+        if (prev_minute != current_time["minute"]):    
+            postDataBy1Min(study_data)
+        
+        # results -> results_array(요소 각각이 딕셔너리인 배열)로 변환
+        results_array = getResultArray(results)
+        # 공부 여부 판별 알고리즘 적용해 study_data에 축적
+        ## TODO 알고리즘 코드 작성
+
+        # 로그 찍기
+        print("===============================")
+        print(f"Frame Per Second: {fps}")
+        print("Current Time: "+str(current_time["hour"])+":"+str(current_time["minute"])+":"+str(time.localtime().tm_sec))
+        for result in results_array:
+            print(result["name"], end=" ")
+            print(round(result["confidence"], 3), end=" ")
+                # 소수점 아래 셋째 자리까지 반올림
+            print("")
+        print(results_array)
+
+if __name__== "__main__":
+    # 해당 파일 실행 시 실행
+
+    model = torch.hub.load('yolov5', 'custom', path='weights/best.pt', source='local', force_reload=True)
+    
+    main()
     pass
