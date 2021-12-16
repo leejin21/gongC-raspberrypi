a6501063ff753af61b55ace88a53c19e8700aa1a
JIN <leejin21c@sookmyung.ac.kr>
2021-12-16 20:26:45 +0900

    add green led test

--- ledTest.py
+++ ledTest.py
@@ -3,9 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
 
 while(True):
     GPIO.output(17,False)
+    GPIO.output(27,True)
     time.sleep(2)
     GPIO.output(17,True)
+    GPIO.output(27,False)
     time.sleep(2)
