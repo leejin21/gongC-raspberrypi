b76516fca2de925671f2218a7db47e2cc91518cf
JIN <leejin21c@sookmyung.ac.kr>
2021-12-16 23:16:10 +0900

    rest api 테스트(aws ec2에 올려둔 서버로)

--- secret.py
+++ secret.py
@@ -3,4 +3,4 @@ def getServerIP():
     return "http://175.115.31.12:5000"
 
 def getTestToken():
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjM3NjU4NjY0LCJleHAiOjUyMzc2NTUwNjR9.E2MQKePtt5A21XvVsrYi2kPxbc-M25b6m7dS-NErfsk"
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjM5NjY0MDkzLCJleHAiOjUyMzk2NjA0OTN9.s9mckKzNW-DD2rYD0YOksfVN08vhq-__XTpLijqNUyg"
